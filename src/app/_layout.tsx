a408bc071f18c501533c1159791323db73ae4822
jessgeo-personal <jessgeojose@gmail.com>
2025-10-04 17:41:57 +0400

    trying to get this to work 2

---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+// src/app/_layout.tsx
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
