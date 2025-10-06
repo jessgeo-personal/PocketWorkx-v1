2ccfee79c57fdb3f2816a654b6ba89ea28b11f4e
jessgeo-personal <jessgeojose@gmail.com>
2025-10-06 13:05:00 +0400

    trying to get this to work 12

--- src/components/ScreenLayout.tsx
+++ src/components/ScreenLayout.tsx
@@ -1,10 +1,11 @@
 // src/components/ScreenLayout.tsx
 
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, SafeAreaView } from 'react-native';
 import SlidingMenu from './SlidingMenu';
 import FloatingMenuButton from './FloatingMenuButton';
 import { colors } from '../utils/theme';
+import logo from '../assets/logo.png';
 
 interface ScreenLayoutProps {
   children: React.ReactNode;
@@ -22,13 +23,16 @@ const ScreenLayout: React.FC<ScreenLayoutProps> = ({ children }) => {
   };
 
   return (
-    <View style={styles.container}>
-      {children}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
+      </View>
+
+      <View style={styles.content}>{children}</View>
 
       <FloatingMenuButton onPress={toggleMenu} isMenuOpen={isMenuVisible} />
-
       <SlidingMenu visible={isMenuVisible} onClose={closeMenu} />
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -36,6 +40,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  header: {
+    height: 60,
+    backgroundColor: colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.surface,
+  },
+  logo: {
+    width: 150,
+    height: 40,
+  },
+  content: {
+    flex: 1,
   },
 });
 
